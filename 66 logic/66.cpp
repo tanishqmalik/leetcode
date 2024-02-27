25cbb046811c061e6931c791eb2b45cc639e3144
tanishq <tanishq.malik31@gmail.com>
2024-02-27 22:36:21 +0530

    nothing

--- 66 logic/66.cpp
+++ 66 logic/66.cpp
@@ -62,4 +62,23 @@ int main(){
 
 
 
+
+
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         for(int i = digits.size() - 1;i >= 0;i--){
+//             if(digits[i] != 9){
+//                 digits[i]++;
+//                 return digits;
+//             }
+//             else
+//                 digits[i] = 0;
+//         }
+//         digits.insert(digits.begin(),1);
+//         return digits;
+//     }
+// };
+
+
 // solved by own
